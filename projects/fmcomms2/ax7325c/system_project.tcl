ca86f798461b6059f66d16c56caaa8e9483a8520
Dmitriy Dyomin <dmitrodem@gmail.com>
2024-02-06 14:50:08 +0300

    Fix typos + add 10/100M Ethernet

--- projects/fmcomms2/ax7325c/system_project.tcl
+++ projects/fmcomms2/ax7325c/system_project.tcl
@@ -7,14 +7,14 @@ source ../../../scripts/adi_env.tcl
 source $ad_hdl_dir/projects/scripts/adi_project_xilinx.tcl
 source $ad_hdl_dir/projects/scripts/adi_board.tcl
 
-adi_project fmcomms2_ax7325b
-adi_project_files fmcomms2_ax7325b [list \
+adi_project fmcomms2_ax7325c
+adi_project_files fmcomms2_ax7325c [list \
   "system_top.v" \
   "system_constr.xdc"\
   "$ad_hdl_dir/library/common/ad_iobuf.v" \
-  "$ad_hdl_dir/projects/common/ax7325b/ax7325b_system_constr.xdc" ]
+  "$ad_hdl_dir/projects/common/ax7325c/ax7325c_system_constr.xdc" ]
 
-adi_project_run fmcomms2_ax7325b
+adi_project_run fmcomms2_ax7325c
 if {[info exists ::env(ADI_SKIP_SYNTHESIS)]} {
     exit
 }
